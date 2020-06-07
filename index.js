7c82118dabf09022b26ec5d736453d7d02c0d3e9
taha uygun <tahauygun@gmail.com>
2020-06-06 23:13:49 -0700

    post routes fix

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const app = express();
 
+const posts = {};
+
 app.get('/post', (req, res) =>{
 
 });
 
 app.post('/post', (req, res) =>{
 
+});
+
+app.listen(4000, () => {
+   console.log('Listening on 4000);
 });
