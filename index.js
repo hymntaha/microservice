8a05a0d96224b04580b0b52889f9c215dbaa2feb
taha uygun <tahauygun@gmail.com>
2020-06-06 23:17:31 -0700

    adding crypto for post

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express');
-
+const {randomBytes} = require('crypto');
 const app = express();
 
 const posts = {};
 
 app.get('/post', (req, res) =>{
-
+    res.send(posts);
 });
 
 app.post('/post', (req, res) =>{
-
+    const id = randomBytes(4).toString('hex');
 });
 
 app.listen(4000, () => {
