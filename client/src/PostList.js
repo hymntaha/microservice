548be0fd463b5c1df890b74ed77bc88d48dbd64b
taha uygun <tahauygun@gmail.com>
2020-06-25 23:48:24 -0700

    adding rendered posts

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -14,7 +14,15 @@ export default () => {
     fetchPosts();
   },[])
 
-  console.log(posts)
+  const renderedPosts = Object.values(post =>{
+    return <div className="card" style={{width:'30%', marginBottom:'20px'}} key={post.id}>
+      <div className="card-body">
+        <h3>{post.title}</h3>
+      </div>
+    </div>
+  });
 
-  return <div />
+  return <div className="d-flex flex-row flex-wrap justify-content-between">
+    {renderedPosts}
+  </div>
 }
