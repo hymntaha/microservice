88bf730d7348a85d12e22b53ca96a62eca33cec1
taha uygun <tahauygun@gmail.com>
2020-06-25 23:42:48 -0700

    adding useEffect and useState for post list

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 
 export default () => {
+  const [posts, setPosts] = useState([]);
   return <div />
 }
